10bb2a6c6e634a185c2dede44741db23df7f4284
mikolaj <mikolaj99x@gmailcom>
2024-09-15 20:09:51 +0200

    Start adding Entities

---
+++ RpgMenager.Domain/Entities/Statistics.cs
@@ -0,0 +1,18 @@
+﻿using RpgMenager.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgMenager.Domain.Entities
+{
+    public class Statistics
+    {
+        int Id { get; set; }
+        int Value { get; set; }  = 0;
+        string Name { get; set; } = default!;
+        string? Description { get; set; } = default;
+        StatisticsEnum statisticsEnum { get; set; } = StatisticsEnum.None;
+    }
+}
---
+++ RpgMenager.Domain/Enums/StatisticsEnums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgMenager.Domain.Enums
+{
+    public enum StatisticsEnum
+    {
+        None = 0,
+        Characteristic = 1,
+        Skill  = 2
+    }
+}
