bdfeeb9c2f900025221800d3ffb8c49ef155b35b
mikolaj <mikolaj99x@gmailcom>
2024-09-18 22:01:44 +0200

    Finaly i fix bostram themes

---
+++ RpgMenager.Application/Mappings/RpgMenagerMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RpgMenager.Application.DtosAnd.Player;
+using RpgMenager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgMenager.Application.Mappings
+{
+    public class RpgMenagerMappingProfile : Profile
+    {
+        public RpgMenagerMappingProfile() {
+            CreateMap<Player, PlayerDto>();
+            CreateMap<PlayerDto, Player>();
+        }
+    }
+}
