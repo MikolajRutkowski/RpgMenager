3f183d24edf528925c45f09343f8180699d5346c
mikolaj <mikolaj99x@gmailcom>
2024-09-24 21:13:46 +0200

    add new model for list

---
+++ RpgMenager/Models/ListOfStatistic.cs
@@ -0,0 +1,18 @@
+﻿using RpgMenager.Application.DtosAnd.Statistic;
+
+namespace RpgMenager.Mvc.Models
+{
+    public class ListOfStatistic
+    {
+        public string Name { get; set; } = default;
+        public int IdOfCharacter { get; set; } = default;
+        public List<StatisticDto> MainList { get; set; } = new List<StatisticDto>();
+        public bool Add(StatisticDto dto)
+        {
+            if (dto.CharacterId == IdOfCharacter) { 
+            
+            }
+            else { return false; }
+        }
+    }
+}
